a58d018401576f61f093a45a402305ba51c45824
Hafizh Fadhlurrohman <hafizh@infradigital.io>
2022-02-20 11:05:36 +0700

    feat: add url_launcher

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DetailScreen extends StatelessWidget {
+  final int index;
   final String title;
-  const DetailScreen({Key? key, required this.title}) : super(key: key);
+  final String imageUrl;
+
+  const DetailScreen({
+    Key? key,
+    required this.title,
+    this.imageUrl = "",
+    this.index = 0,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          child: IconButton(
+            icon: const Icon(
+              Icons.account_balance_wallet_outlined,
+              size: 60,
+            ),
+            onPressed: () async {
+              if (!await launch("https://google.com")) {
+                throw 'Could not launch https://google.com';
+              }
+            },
+          ),
+        ),
+        color: Colors.amber,
+      ),
       body: Center(
-        child: Text(
-          title,
-          style: TextStyle(
-              fontSize: 100, color: Colors.pink, fontWeight: FontWeight.bold),
+        child: ListView(
+          children: [
+            Hero(
+              tag: index,
+              child: Image.asset(imageUrl),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 0, 0),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.pink,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -39,61 +39,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   void initState() {
     super.initState();
-    _incrementCounter();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("SMK BISA"),
+        title: const Text("SMK BISA"),
         backgroundColor: Colors.blue,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
+            SizedBox(
               height: 700,
               child: ListView.builder(
                 itemCount: 10,
@@ -104,7 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
                         context,
                         MaterialPageRoute(
                           builder: (context) {
-                            return DetailScreen(title: "Ini Tile Ke : $index");
+                            return DetailScreen(
+                              index: index,
+                              title: "Ini Tile Ke : $index",
+                              imageUrl: "assets/images/assets.jpeg",
+                            );
                           },
                         ),
                       );
@@ -113,9 +79,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       title: Text(
                         "Ini Tile Ke : $index",
                       ),
-                      subtitle: Text("Ini Subtitle"),
-                      leading: Image.asset("assets/images/assets.jpeg"),
-                      trailing: Icon(Icons.backpack),
+                      subtitle: const Text("Ini Subtitle"),
+                      leading: Hero(
+                        tag: index,
+                        child: Image.asset("assets/images/assets.jpeg"),
+                      ),
+                      trailing: const Icon(Icons.backpack),
                     ),
                   );
                 },
